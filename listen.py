d3aa9a26d6163fe4c9a4661af19bb9586570dc30
Andrei Serbanescu <andrei.serbanescu16@imperial.ac.uk>
2020-04-08 21:57:15 +0300

    run segmentation on request

--- listen.py
+++ listen.py
@@ -8,6 +8,7 @@ import sys
 import SimpleITK as sitk
 import numpy as np
 import json
+import time
 
 class CommandRequestHandler(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -61,19 +62,29 @@ def mark_yourself_ready():
     sb.call([cmd], shell=True)
 
 
-def convert(param_dict):
-    # TODO implement
-    print("### converter got parameters".format(param_dict))
-
-    result_dict = {}
-    return result_dict
-
+# PRE: file is .nii.gz
 def segment_muscle(param_dict):
-    print("### converter got parameters".format(param_dict))
+    print("### converter got parameters {}".format(param_dict))
 
-    # TODO check file is .nii.gz not .dcm
-    # TODO implement
-    result_dict = {}
+    source_file = param_dict["source_file"][0]
+    model_name = "MuscleNC"
+
+    segment_command = "python3 /app/Inference.py --single_file {} --result_root {} --model {}"\
+                         .format(source_file, "/tmp", model_name)
+    sb.call([segment_command], shell=True)
+
+    # remove from the end of source file ".nii.gz"
+    volume_name = os.path.split(source_file)[1]
+    source_file_name = volume_name[:len(volume_name) - 7]
+
+    tmp_out_file = os.path.join("/tmp", source_file_name  + "_MuscleNC.nii.gz")
+    data_share   = os.environ["DATA_SHARE_PATH"]
+    output_name  = "ct-muscle-segment-" + str(time.time()) + ".nii.gz"
+
+    move_command = "mv {} {}/{}".format(tmp_out_file, data_share, output_name)
+    sb.call([move_command], shell=True)
+
+    result_dict = {"segmentation": output_name}
     return result_dict
 
 def setup_logging():
